296f4db7f10efb297e1cf57a118e23f2e36f27c3
Abdulrahman Khaledi <85647212+abdukhaledi@users.noreply.github.com>
2021-12-31 01:39:22 +0300

    Add files via upload

---
+++ LM13.X/build/default/production/Temperature_Display.p1.d
@@ -0,0 +1,5 @@
+build/default/production/Temperature_Display.p1:  \
+Temperature_Display.c  \
+Configuration_Header_File.h  \
+LCD_16x2_8-bit_Header_File.h  \
+PIC18F4550_ADC_Header_File.h 
---
+++ LM13.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=LM13_Debugging.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LM13_Debugging.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=lm13debugging.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lm13debugging.x.tar
